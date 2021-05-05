60661a16314063ec02510067926ceeef2e79e98c
Xavier Valarino <xvalarino@pinterest.com>
2021-05-05 14:07:18 -0700

    Add brew package install script

---
+++ scripts/brew-packages.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# Core
+CORE=(
+  bat
+  fzf  
+  git 
+  git-delta 
+  m-cli 
+  node 
+  nvm 
+  pidof
+  tldr 
+  trash 
+  vim 
+  shellcheck
+  the_silver_searcher
+  tree
+  utimer
+)
+
+CASKS=(
+  hammerspoon 
+  iterm2 
+  karabiner-elements 
+  logitech-options 
+)
+
+printf "%s\n" "Installing core packages..."
+brew install ${CORE[@]}
+
+printf "%s\n" "Installing casks..."
+brew install ${CASKS[@]}
+
+
